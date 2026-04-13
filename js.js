701136008e4c65b7cad1f82adb3d1f9bbd5f0c54
Forkrandt <hfhknvoih@gmail.com>
2026-04-13 21:18:16 +0300

    1

--- js.js
+++ js.js
@@ -1,2 +1,4 @@
 const min = document.getElementById("min")
-const sec= document.getElementById("sec")
+const sec = document.getElementById("sec")
+const btn_start = document.getElementById("open")
+const btn_reset = document.getElementById("reset")
